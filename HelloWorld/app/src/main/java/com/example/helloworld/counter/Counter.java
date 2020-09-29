d9bfa8d08d74a4753c1d4a03db4b56b49b116728
cpanou <65117528+cpanou@users.noreply.github.com>
2020-09-30 01:21:25 +0300

    fixes count in Counter

--- HelloWorld/app/src/main/java/com/example/helloworld/counter/Counter.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/counter/Counter.java
@@ -1,10 +1,13 @@
 package com.example.helloworld.counter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.helloworld.MainActivity;
 
 import org.w3c.dom.Text;
 
@@ -12,15 +15,21 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class Counter {
+    private static final String LOG_TAG = Counter.class.getSimpleName();
+
     private Context context;
     private Integer mCount;
     private TextView counterText;
 
-    private Consumer<Integer> updateCounterText = (count) -> counterText.setText(String.valueOf(count));
+    private Consumer<Integer> updateCounterText = (count) -> {
+        Log.d(LOG_TAG,"Counter Updated %s" + count);
+        counterText.setText(String.valueOf(count));
+    };
 
     public Counter(Context context, TextView counterText) {
         this.counterText = counterText;
         this.context = context;
+        this.mCount = 0;
     }
 
     public Consumer<View> getCountUp() {
