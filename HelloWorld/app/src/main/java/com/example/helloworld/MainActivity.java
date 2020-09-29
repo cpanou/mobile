00a6ccb3817a6214a18c4079df07b4c257fea4bf
cpanou <65117528+cpanou@users.noreply.github.com>
2020-09-30 02:29:43 +0300

    colorz

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.helloworld.counter.Counter;
-import com.example.helloworld.counter.EventBinder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,21 +32,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void bindCounterView() {
-        //(1) The Counter object needs the TextView
-        TextView counterText = findViewById(R.id.show_count);
-        //(2) Create the Counter Object
-        Counter counter = new Counter(this, counterText);
-
-        //(3) Get references to the buttons
         Button toastButton = findViewById(R.id.toast_button);
         Button countButton = findViewById(R.id.count_button);
         Button zeroButton = findViewById(R.id.zero_button);
-
-        //(4) The eventBinder sets a click listener to a button following various methods.
-        //We pass in a button and a Consumer so that they are bound together
-        EventBinder.setButtonHandler(toastButton,  counter.getShowToast());
-        EventBinder.setButtonHandler(countButton,  counter.getCountUp());
-        EventBinder.setButtonHandler(zeroButton,  counter.getZeroCounter());
+        TextView counterText = findViewById(R.id.show_count);
+        Counter.bind(this, countButton, zeroButton, toastButton, counterText);
     }
 
 }
--- HelloWorld/app/src/main/java/com/example/helloworld/counter/Counter.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/counter/Counter.java
@@ -1,8 +1,8 @@
 package com.example.helloworld.counter;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,40 +11,47 @@ import com.example.helloworld.R;
 import java.util.function.Consumer;
 
 public class Counter {
-    private static final String LOG_TAG = Counter.class.getSimpleName();
-
     private Context context;
-    private Integer mCount;
+    private Integer mCount = 0;
+    private Button countButton;
+    private Button zeroButton;
+    private Button toastButton;
     private TextView counterText;
 
-    private Consumer<Integer> updateCounterText = (count) -> {
-        Log.d(LOG_TAG, "Counter Updated " + count);
-        counterText.setText(String.valueOf(count));
+    private Consumer<Integer> updateCounterText = (count) -> counterText.setText(String.valueOf(count));
+    private Consumer<View> countUp = (view) -> {
+        view.setBackgroundColor(mCount % 2== 0 ? context.getColor(R.color.colorPrimary) : context.getColor(R.color.colorSecondary));
+        if(mCount ==0) zeroButton.setBackgroundColor(context.getColor(R.color.colorWarn));
+        updateCounterText.accept(++mCount);
     };
+    private Consumer<View> zeroCounter = (view) -> updateCounterText.accept(mCount = 0);
+    private Consumer<View> showToast = (view) -> Toast.makeText(context, "Count: " + mCount, Toast.LENGTH_LONG).show();
 
-    public Counter(Context context, TextView counterText) {
+    public Counter(Context context, Button countButton, Button zeroButton, Button toastButton, TextView counterText) {
+        this.countButton = countButton;
+        this.zeroButton = zeroButton;
+        this.toastButton = toastButton;
         this.counterText = counterText;
         this.context = context;
-        this.mCount = 0;
     }
 
-    public Consumer<View> getCountUp() {
-        return (view) -> {
-            view.setBackgroundColor(mCount % 2 == 0 ? context.getColor(R.color.colorPrimary) : context.getColor(R.color.colorSecondary));
-            updateCounterText.accept(++mCount);
-        };
+    public static void bind(Context context, Button countButton, Button zeroButton, Button toastButton, TextView counterText) {
+        new Counter(context, countButton, zeroButton, toastButton, counterText).setHandlers();
     }
 
-    public Consumer<View> getZeroCounter() {
-        return (view) -> updateCounterText.accept(mCount = 0);
+
+    private Counter setHandlers() {
+        this.toastButton.setOnClickListener(showToast::accept);
+        this.countButton.setOnClickListener(countUp::accept);
+        this.zeroButton.setOnClickListener(zeroCounter::accept);
+        return this;
     }
 
-    public Consumer<View> getShowToast() {
-        return (view) -> Toast.makeText(context, "Count: " + mCount, Toast.LENGTH_LONG).show();
-    }
-
-    public Integer getmCount() {
-        return mCount;
+    private Counter withEventHandler() {
+        EventBinder.setButtonHandler(this.toastButton, this.showToast);
+        EventBinder.setButtonHandler(this.countButton, this.countUp);
+        EventBinder.setButtonHandler(this.zeroButton, this.zeroCounter);
+        return this;
     }
 
 }
